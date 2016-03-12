5cd759e35884cb52256d7caf2932269c7ffddd42
Martin Chuckeles <martin@jakubik.info>
2016-03-12 21:36:50 +0100

    Generate StaticPages controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require none
-//= require none_ujs
 //= require turbolinks
 //= require_tree .
