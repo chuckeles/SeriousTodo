ab1fcc94b2a940a5db9c4fb78691329b3c445431
Martin Chuckeles <martin@jakubik.info>
2016-03-20 19:50:02 +0100

    Start with user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,15 @@
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with a name and an email" do
+    user = User.new(name: "chuckeles", email: "me@chuckeles.me")
+
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.new(email: "me@chuckeles.me")
+
+    expect(user).to_not be_valid
+  end
+
 end
