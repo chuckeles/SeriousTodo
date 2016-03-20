42520528773048309231644b32b5490e752505c4
Martin Chuckeles <martin@jakubik.info>
2016-03-20 21:52:04 +0100

    Make specs use user created in before

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,49 +1,54 @@
 RSpec.describe User do
 
-  it "is valid with a name and an email" do
-    user = User.new(name: "chuckeles", email: "me@chuckeles.me")
+  before do
+    @user = User.new(name: "chuckeles", email: "me@chuckeles.me", password: "foo", password_confirmation: "foo")
+  end
 
-    expect(user).to be_valid
+  it "is valid with a name and an email" do
+    expect(@user).to be_valid
   end
 
   it "is invalid without a name" do
-    user = User.new(email: "me@chuckeles.me")
+    @user.name = ""
 
-    expect(user).to_not be_valid
+    expect(@user).to_not be_valid
   end
 
   it "is invalid with a name that is too long" do
-    user = User.new(name: "c" * 33, email: "me@chuckeles.me")
+    @user.name = "c" * 33
 
-    expect(user).to_not be_valid
+    expect(@user).to_not be_valid
   end
 
   it "is invalid with an existing name" do
-    user1 = User.new(name: "chuckeles", email: "me1@chuckeles.me")
-    user2 = User.new(name: "chuckeles", email: "me2@chuckeles.me")
-
-    user1.save
+    @user.save
+    user2 = @user.dup
 
     expect(user2).to_not be_valid
   end
 
   it "is invalid without an email" do
-    user = User.new(name: "chuckeles")
+    @user.email = ""
 
-    expect(user).to_not be_valid
+    expect(@user).to_not be_valid
   end
 
   it "is invalid with an email that is too long" do
-    user = User.new(name: "chuckeles", email: "c" * 243 + "@chuckeles.me")
+    @user.email = "c" * 243 + "@chuckeles.me"
 
-    expect(user).to_not be_valid
+    expect(@user).to_not be_valid
   end
 
   it "is invalid with an invalid email" do
-    user1 = User.new(name: "chuckeles", email: "me")
-    user2 = User.new(name: "chuckeles", email: "me.me")
-    user3 = User.new(name: "chuckeles", email: "me@me")
-    user4 = User.new(name: "chuckeles", email: "me@me..me")
+    user1 = @user.dup
+    user2 = @user.dup
+    user3 = @user.dup
+    user4 = @user.dup
+
+    user1.email = "me"
+    user2.email = "me.me"
+    user3.email = "me@me"
+    user4.email = "me@me..me"
 
     expect(user1).to_not be_valid
     expect(user2).to_not be_valid
@@ -52,22 +57,27 @@ RSpec.describe User do
   end
 
   it "is invalid with an existing email" do
-    user1 = User.new(name: "chuckeles1", email: "me@chuckeles.me")
-    user2 = User.new(name: "chuckeles2", email: "me@chuckeles.me")
-    user3 = User.new(name: "chuckeles3", email: "ME@chuckeles.me")
+    user1 = @user.dup
+    user2 = @user.dup
+    user3 = @user.dup
 
-    user1.save
+    user1.email = "me@chuckeles.ME"
+    user2.email = "me@chuckeles.me"
+    user3.email = "ME@chuckeles.me"
 
+    @user.save
+
+    expect(user1).to_not be_valid
     expect(user2).to_not be_valid
     expect(user3).to_not be_valid
   end
 
   it "has email downcased in the database" do
-    user = User.new(name: "chuckeles", email: "ME@chuckeles.ME")
+    @user.email = "ME@chuckeles.ME"
 
-    user.save
+    @user.save
 
-    expect(user.reload.email).to eq("me@chuckeles.me")
+    expect(@user.reload.email).to eq("me@chuckeles.me")
   end
 
 end
