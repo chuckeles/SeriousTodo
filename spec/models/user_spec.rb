bdd54b42935646cebb9cab055af72909ba64e03b
Martin Chuckeles <martin@jakubik.info>
2016-03-20 20:04:43 +0100

    Validate maximum name and email length

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,32 @@ RSpec.describe User do
     expect(user).to_not be_valid
   end
 
+  it "is invalid with a name that is too long" do
+    user = User.new(name: "c" * 33, email: "me@chuckeles.me")
+
+    expect(user).to_not be_valid
+  end
+
   it "is invalid without an email" do
     user = User.new(name: "chuckeles")
 
     expect(user).to_not be_valid
+  end
+
+  it "is invalid with an email that is too long" do
+    user = User.new(name: "chuckeles", email: "c" * 243 + "@chuckeles.me")
+
+    expect(user).to_not be_valid
+  end
+
+  xit "is invalid with an invalid email" do
+    user1 = User.new(name: "chuckeles", email: "me")
+    user2 = User.new(name: "chuckeles", email: "me.me")
+    user3 = User.new(name: "chuckeles", email: "me@me")
+
+    expect(user1).to_not be_valid
+    expect(user2).to_not be_valid
+    expect(user3).to_not be_valid
   end
 
 end
