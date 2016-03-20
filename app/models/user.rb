559193c579f7d4e435f91d4872c4a69d1f9c4b32
Martin Chuckeles <martin@jakubik.info>
2016-03-20 21:03:13 +0100

    Convert emails to downcase before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 32 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
+  before_save do
+    self.email = email.downcase
+  end
+
 end
