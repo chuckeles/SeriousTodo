db78a9fa4e49aaa687d7946b78d5e423cb6c091b
Martin Chuckeles <martin@jakubik.info>
2016-03-24 23:17:56 +0100

    Add user controller show spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,23 @@
 RSpec.describe UsersController do
 
+  describe "GET show" do
+    before do
+      @user = User.create(name: "chuckeles", email: "me@chuckeles.me", password: "foobaz", password_confirmation: "foobaz")
+    end
 
+    it "gets show" do
+      get :show, { id: @user.id }
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "has show template" do
+      get :show, { id: @user.id }
+
+      expect(response).to render_template(:show)
+    end
+
+    # TODO: test if it shows correct user
+  end
 
 end
