4c8eadc245b6b1f205bf5e90d7ee154fb2b0a0d3
Martin Chuckeles <martin@jakubik.info>
2016-03-24 23:05:18 +0100

    Add empty signup page

--- spec/features/home_menu_spec.rb
+++ spec/features/home_menu_spec.rb
@@ -10,4 +10,12 @@ RSpec.feature "Homepage navigation" do
     expect(page).to have_current_path("/about")
   end
 
+  scenario "User clicks signup link" do
+    visit "/"
+
+    click_link "Sign up"
+
+    expect(page).to have_current_path("/signup")
+  end
+
 end
