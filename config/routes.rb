c0749d5ce358f745e8bc1b5869d5225ee802f479
Martin Chuckeles <martin@jakubik.info>
2016-03-26 14:11:45 +0100

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root "static_pages#home"
-  get "about" => "static_pages#about"
-  get "signup" => "users#new"
+  get "about", to: "static_pages#about"
+  get "signup", to: "users#new"
 
   resources :users
 
