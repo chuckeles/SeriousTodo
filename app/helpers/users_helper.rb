34680d6bd5e2e967454b584baddbedc10d71ed29
Martin Chuckeles <martin@jakubik.info>
2016-03-26 15:11:42 +0100

    Add size as a parameter to gravatar_for

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 100 })
     id = Digest::MD5::hexdigest(user.email.downcase)
-    url = "https://secure.gravatar.com/avatar/#{id}?s=200"
+    url = "https://secure.gravatar.com/avatar/#{id}?s=#{options[:size]}"
 
     image_tag(url, alt: user.name)
   end
