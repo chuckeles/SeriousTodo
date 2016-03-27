b197c6f32c2cd150d930f9ed247c14ccaa6832d9
Martin Chuckeles <martin@jakubik.info>
2016-03-27 17:18:07 +0200

    Make logout work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      login(@user)
+      redirect_to current_user
     else
       render :new
     end
--- spec/features/menu_spec.rb
+++ spec/features/menu_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe "Main menu" do
     @user = User.create(name: "chuckeles", email: "me@chuckeles.me", password: "foobaz", password_confirmation: "foobaz")
   end
 
+  let :login do
+    visit login_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Login"
+  end
+
   it "has a link to about" do
     visit root_path
 
@@ -28,18 +35,34 @@ RSpec.describe "Main menu" do
     expect(page).to have_current_path(new_user_path)
   end
 
-  it "has profile links and does not have signup and login links when a user is logged in" do
-    visit login_path
-    fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password
-    click_button "Login"
-
+  it "has profile and logout links and does not have signup and login links when a user is logged in" do
+    login
     visit root_path
 
     expect(page).to have_content("Profile")
     expect(page).to have_content("Logout")
     expect(page).to_not have_content("Signup")
     expect(page).to_not have_content("Login")
+  end
+
+
+  it "has a link to logged user's profile" do
+    login
+    visit root_path
+
+    click_link "Profile"
+
+    expect(page).to have_current_path(user_path(@user))
+  end
+
+  it "has a logout link that logs the user out" do
+    login
+    visit root_path
+
+    click_link "Logout"
+
+    expect(page).to have_content("Login")
+    expect(page).to_not have_content("Logout")
   end
 
 end
