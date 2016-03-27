749bfc897bcff40fe6919e37d06e32689d1787d7
Martin Chuckeles <martin@jakubik.info>
2016-03-27 21:23:55 +0200

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  root "static_pages#home"
-  get "about", to: "static_pages#about"
+  root              to: "static_pages#home"
+  get    "about",   to: "static_pages#about"
 
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  get "logout", to: "sessions#destroy"
-  delete "logout", to: "sessions#destroy"
+  get    "login",   to: "sessions#new"
+  post   "login",   to: "sessions#create"
+  get    "logout",  to: "sessions#destroy"
+  delete "logout",  to: "sessions#destroy"
 
   resources :users, param: :name
 
