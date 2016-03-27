03d32a266369b621f8358abc99842f2ca3a7a8b3
Martin Chuckeles <martin@jakubik.info>
2016-03-27 16:11:44 +0200

    Login now creates a new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user and user.authenticate(params[:session][:password])
-
+      login(user)
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email and password combination"
       render :new
