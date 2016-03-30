7c24b1a43cab7f5f62c6334c61a73da7c2739fe3
Martin Chuckeles <martin@jakubik.info>
2016-03-30 17:03:44 +0200

    Delete sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
 
-  root              to: "static_pages#home"
-  get    "about",   to: "static_pages#about"
+  root               to: "static_pages#home"
+  get "about",       to: "static_pages#about"
 
   devise_for :users
 
-  get    "login",   to: "sessions#new"
-  post   "login",   to: "sessions#create"
-  get    "logout",  to: "sessions#destroy"
-  delete "logout",  to: "sessions#destroy"
-
-  resources :users, param: :name
+  resources  :users, param: :name
 
 end
