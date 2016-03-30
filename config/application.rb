871bcee29a556c75238f60b553b6f67b7dd48c01
Martin Chuckeles <martin@jakubik.info>
2016-03-30 16:21:29 +0200

    Set domain name

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,13 @@ module SeriousTodo
 
     config.active_record.raise_in_transactional_callbacks = true
     config.generators do |g|
-      g.assets(false)
-      g.test_framework(:rspec)
-      g.template_engine(:slim)
+      g.assets false
+      g.template_engine :slim
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+      g.helper_specs false
+      g.request_specs false
     end
 
     config.x.name = "Serious Todo"
