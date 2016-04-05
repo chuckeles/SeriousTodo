7abd3af6be67f976f9fd8ea137d0a447e7d849e9
Martin Chuckeles <martin@jakubik.info>
2016-04-05 14:44:30 +0200

    Get the Todoist token

--- app/controllers/todo_apps_controller.rb
+++ app/controllers/todo_apps_controller.rb
@@ -1,13 +1,29 @@
+require "net/http"
+require "json"
+
 class TodoAppsController < ApplicationController
 
   def connect
     redirect_to new_user_session_path unless user_signed_in?
 
-    @todoist_url = "https://todoist.com/oauth/authorize?client_id=3730f95dc06d48608155f8fb37484ba1&scope=data:read&state=" + Rails.application.secrets.state
+    @todoist_url = "https://todoist.com/oauth/authorize?client_id=#{ Rails.application.secrets.todoist_id }&scope=data:read&state=#{ Rails.application.secrets.state }"
   end
 
   def todoist
-    flash[:notice] = "Todoist successfully connected."
+    state = params[:state]
+
+    if state != Rails.application.secrets.state
+      flash[:danger] = "State does not match! Please try again."
+    else
+      code = params[:code]
+
+      url = URI("https://todoist.com/oauth/access_token")
+      result = Net::HTTP.post_form(url, client_id: Rails.application.secrets.todoist_id, client_secret: Rails.application.secrets.todoist_secret, code: code)
+
+      json_body = JSON.parse(result.body)
+      p json_body["access_token"]
+      flash[:notice] = "Todoist successfully connected."
+    end
 
     redirect_to todo_apps_connect_path
   end
