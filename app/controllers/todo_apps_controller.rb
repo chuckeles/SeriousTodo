f076b7c29349ac6faa9325c2362726ed81ff2332
Martin Chuckeles <martin@jakubik.info>
2016-04-05 10:36:26 +0200

    Add Todoist connect link

--- app/controllers/todo_apps_controller.rb
+++ app/controllers/todo_apps_controller.rb
@@ -2,6 +2,14 @@ class TodoAppsController < ApplicationController
 
   def connect
     redirect_to new_user_session_path unless user_signed_in?
+
+    @todoist_url = "https://todoist.com/oauth/authorize?client_id=3730f95dc06d48608155f8fb37484ba1&scope=data:read&state=" + Rails.application.secrets.state
+  end
+
+  def todoist
+    flash[:notice] = "Todoist successfully connected."
+
+    redirect_to todo_apps_connect_path
   end
 
 end
