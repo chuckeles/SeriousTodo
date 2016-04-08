8c33a77e862cc34963d8255996ec52eddac58510
Martin Chuckeles <martin@jakubik.info>
2016-04-08 19:58:24 +0200

    Add sql insert

--- app/models/todo_app.rb
+++ app/models/todo_app.rb
@@ -3,4 +3,20 @@ class TodoApp < ActiveRecord::Base
   belongs_to :user
   validates :token, presence: true
 
+  def sql_insert
+    return false unless valid?
+
+    created_at = created_at || DateTime.now
+    updated_at = updated_at || created_at
+
+    ActiveRecord::Base.connection.execute <<-SQL
+      begin;
+
+      insert into todo_apps (user_id, token, created_at, updated_at)
+      values ('#{user_id}', '#{token}', '#{created_at}', '#{updated_at}');
+
+      commit;
+    SQL
+  end
+
 end
--- spec/models/todo_app_spec.rb
+++ spec/models/todo_app_spec.rb
@@ -19,4 +19,14 @@ RSpec.describe TodoApp do
     expect(app).to_not be_valid
   end
 
+  describe "SQL methods" do
+    it "can create a todo app" do
+      app = TodoApp.new(user_id: @user.id, token: "secret_token")
+
+      app.sql_insert
+
+      expect(TodoApp.first.token).to eq(app.token)
+    end
+  end
+
 end
