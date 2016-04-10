d4d29de91996878e57372602d20b5e2166c8f6c8
Martin Chuckeles <martin@jakubik.info>
2016-04-10 18:56:11 +0200

    Move authorization to Todoist class

--- app/integrations/todoist.rb
+++ app/integrations/todoist.rb
@@ -1,21 +1,38 @@
 class Todoist
 
   @@authorize_url = "https://todoist.com/oauth/authorize"
+  @@token_url = "https://todoist.com/oauth/access_token"
   @@sync_url = "https://todoist.com/API/v6/sync"
 
-  def connected?(user)
+  def self.connected?(user)
+    user.todo_apps.any?
   end
 
-  def authorize_url
+  def self.authorize_url
+    @@authorize_url + "?client_id=#{ Rails.application.secrets.todoist_id }&scope=data:read&state=#{ Rails.application.secrets.state }"
   end
 
-  def authorize(user)
+  def self.authorize(user, state, code, flash)
+    if state != Rails.application.secrets.state
+      flash[:danger] = "State does not match! Please try again."
+    else
+      url = URI(@@token_url)
+      result = Net::HTTP.post_form(url, client_id: Rails.application.secrets.todoist_id, client_secret: Rails.application.secrets.todoist_secret, code: code)
+      json_body = JSON.parse(result.body)
+
+      if json_body["error"]
+        flash[:danger] = "Error getting the token! Please try again."
+      else
+        user.todo_apps.create(token: AES.encrypt(json_body["access_token"], Rails.application.secrets.secret_key_base))
+        flash[:notice] = "Todoist successfully connected."
+      end
+    end
   end
 
-  def items(user)
+  def self.items(user)
   end
 
-  def item(user)
+  def self.item(user)
   end
 
 end
