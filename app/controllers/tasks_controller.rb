e141a1bf0088937399ee6c7b8c4553ee88425132
Martin Chuckeles <martin@jakubik.info>
2016-04-10 09:57:01 +0200

    Add tasks spec

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
 
   before_action :authenticate_user!
+  before_action do
+    if current_user.todo_apps.empty?
+      flash[:alert] = "You have to connect a todo app first."
+      redirect_to todo_apps_connect_path
+    end
+  end
 
   def index
-    
+
   end
 
 end
---
+++ spec/features/tasks_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe "Tasks" do
+
+  before do
+    @user = User.new(name: "chuckeles", email: "me@chuckeles.me", password: "foobaaaz", password_confirmation: "foobaaaz")
+    @user.skip_confirmation!
+    @user.save!
+  end
+
+  it "redirect to login when no user is logged in" do
+    visit tasks_path
+
+    expect(page).to have_current_path(new_user_session_path)
+  end
+
+  it "redirect to connect when no app is connected" do
+    log_in
+    visit tasks_path
+
+    expect(page).to have_current_path(todo_apps_connect_path)
+  end
+
+  def log_in
+    visit new_user_session_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Log in"
+  end
+
+  def connect_todo_app
+    @user.todo_apps.create(token: "what_is_this_huh")
+  end
+
+end
