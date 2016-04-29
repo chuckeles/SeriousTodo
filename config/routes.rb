d77bcb1abb34cc6fe729aa727ff89c82ef29de5b
Martin Chuckeles <martin@jakubik.info>
2016-04-29 12:08:09 +0200

    Only show calls to action and about to new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-  root                     to: "static_pages#home"
-  get "about",             to: "static_pages#about"
+  root                                to: "static_pages#home"
+  get "about",                        to: "static_pages#about"
 
   devise_for :users
 
-  get "users/:name",                  to: "users#show", as: :user
+  get "users/:name",                  to: "users#show",                  as: :user
   get "todo-apps/connect",            to: "todo_apps#connect"
   get "todo-apps/connect/todoist",    to: "todo_apps#connect_todoist"
   get "todo-apps/disconnect",         to: "todo_apps#disconnect"
   get "todo-apps/disconnect/todoist", to: "todo_apps#disconnect_todoist"
   get "tasks",                        to: "tasks#index"
-  get "tasks/:id",                    to: "tasks#show", as: :task
+  get "tasks/:id",                    to: "tasks#show",                  as: :task
 
 end
