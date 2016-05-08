fa707f0d881fe05430f6c2634d54ec4ffd4d373b
Martin Chuckeles <martin@jakubik.info>
2016-05-08 13:59:03 +0200

    Remove fixtures from config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,6 @@ require "devise"
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.filter_rails_from_backtrace!
   config.infer_spec_type_from_file_location!
