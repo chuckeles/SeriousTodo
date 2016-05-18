14689326044a0be853efaa4f3029a409ac830225
Martin Chuckeles <martin@jakubik.info>
2016-05-18 13:13:47 +0200

    Add Pledge validations

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,2 +1,6 @@
 class Pledge < ActiveRecord::Base
+
+  validates :task_id, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+
 end
--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -1,7 +1,47 @@
-RSpec.fdescribe Pledge do
+RSpec.describe Pledge do
 
   context "with a task id and a positive amount" do
     it { is_expected.to be_valid }
+  end
+
+  context "without a task id" do
+    subject do
+      pledge = valid_pledge.dup
+      pledge.task_id = nil
+      pledge
+    end
+
+    it { is_expected.to_not be_valid }
+  end
+
+  context "without an amount" do
+    subject do
+      pledge = valid_pledge.dup
+      pledge.amount = nil
+      pledge
+    end
+
+    it { is_expected.to_not be_valid }
+  end
+
+  context "with a negative amount" do
+    subject do
+      pledge = valid_pledge.dup
+      pledge.amount = -10
+      pledge
+    end
+
+    it { is_expected.to_not be_valid }
+  end
+
+  context "with a string amount" do
+    subject do
+      pledge = valid_pledge.dup
+      pledge.amount = "string"
+      pledge
+    end
+
+    it { is_expected.to_not be_valid }
   end
 
   subject(:valid_pledge) do
