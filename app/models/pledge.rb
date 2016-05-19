1c301a0d2054312b8f0afe419c1e8971966b3620
Martin Chuckeles <martin@jakubik.info>
2016-05-19 11:21:29 +0200

    Write the processing code

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,3 +1,5 @@
+require_relative "../utilities/todoist"
+
 class Pledge < ActiveRecord::Base
 
   validates :task_id, presence: true, uniqueness: true
@@ -6,7 +8,34 @@ class Pledge < ActiveRecord::Base
   belongs_to :user
 
   def self.process_all
+    success_count = 0
+    fail_count = 0
 
+    tasks = {}
+    all.each do |pledge|
+      unless tasks[pledge.user_id]
+        tasks[pledge.user_id] = {}
+        items = Todoist.items(User.find(pledge.user_id), {})
+        items.each do |item|
+          tasks[pledge.user_id][item[:id]] = item
+        end
+      end
+
+      p tasks
+      if not tasks[pledge.user_id][pledge.task_id]
+        # Pledge.delete(pledge.id)
+        success_count += 1
+        p "Pledge #{pledge.id} deleted, the task is complete."
+      elsif tasks[pledges.user_id][pledge.task_id][:due] < DateTime.now
+        fail_count += 1
+        p "Task #{tasks[pledge.user_id][pledge.task_id][:content]} overdue! Charge the user!!!"
+      else
+        p "Pledge for task #{tasks[pledge.user_id][pledge.task_id][:content]} is still valid."
+      end
+    end
+
+    p "Successful pledges: #{success_count}"
+    p "Failed pledges (income): #{fail_count}"
   end
 
 end
