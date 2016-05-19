dacd5c702fa770df840949ef0ee96f7faf5bed05
Martin Chuckeles <martin@jakubik.info>
2016-05-19 10:14:33 +0200

    Add scheduler rake task

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,4 +3,8 @@ class Pledge < ActiveRecord::Base
   validates :task_id, presence: true, uniqueness: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 2 }
 
+  def self.process_all
+
+  end
+
 end
