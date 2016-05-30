ae7678f2c84ff4f37e2cc3dc7117f15e537ff58b
Martin Chuckeles <martin@jakubik.info>
2016-05-30 20:13:54 +0200

    Hound is right, status is not required

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -2,6 +2,6 @@ require "segment/analytics"
 
 Analytics = Segment::Analytics.new({
   write_key: Rails.application.secrets.segment_key,
-  on_error: Proc.new { |status, msg| print msg },
+  on_error: Proc.new { |_, msg| print msg },
   stub: Rails.env.test?
 })
